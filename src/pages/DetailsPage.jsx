b012c359e45ffeaf512aaea0dd6598102e7c2053
Anton Golomazov <a.golomazov@id-mt.ru>
2021-10-24 22:52:17 +0300

    Complete HomePage

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,7 +1,37 @@
-export const DetailsPage = ({ match }) => {
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { IoArrowBack } from "react-icons/io5";
+import { useParams, useHistory } from 'react-router-dom';
+import { Button } from '../components/Button';
+import { searchByCountry } from '../config';
+
+
+export const DetailsPage = () => {
+  const { country: countryName } = useParams();
+  const { goBack, push } = useHistory();
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    async function getCountryData() {
+      const responseLink = searchByCountry(countryName);
+
+      try {
+        const response = await axios.get(responseLink);
+        setCountry(response.data[0]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
+    getCountryData();
+  }, [countryName]);
+
   return (
     <div>
-      <h1>Details Page - { match.params.country }</h1>
+      <Button onClick={goBack}>
+        <IoArrowBack /> Back
+      </Button>
+      <h1>Details Page - {countryName}</h1>
     </div>
-  )
+  );
 }
